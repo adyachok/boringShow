d0b35e753738830fe157e3aaca851c501cd6eb52
Andras Gyacsok <andras.gyacsok@boehringer-ingelheim.com>
2019-11-08 14:41:29 +0100

    Animation

--- lib/hn_bloc.dart
+++ lib/hn_bloc.dart
@@ -1,59 +1,86 @@
+import 'dart:async';
 import 'dart:collection';
 
 import 'package:hacker_news/article.dart';
 import 'package:rxdart/rxdart.dart';
 import 'package:http/http.dart' as http;
 
+enum StoriesType {
+  topStories,
+  newStories,
+}
+
 class HackerNewsBloc {
+  Stream<bool> get isLoading => _isLoadingSubject.stream;
+  final _isLoadingSubject = BehaviorSubject<bool>.seeded(false);
+
   Stream<List<Article>> get articles => _articlesSubject.stream;
   final _articlesSubject = BehaviorSubject<UnmodifiableListView<Article>>();
-
+  Sink<StoriesType> get storiesType => _storiesTypeController.sink;
+  final _storiesTypeController = StreamController<StoriesType>();
   var _articles = <Article>[];
-  
-    HackerNewsBloc() {
-      _updateArticles().then((_){
-        _articlesSubject.add(UnmodifiableListView(_articles));
-      });
-    }
-  
-    List<int> _ids = [
-      21440526,
-      21440508,
-      21440582,
-      21437334,
-      21439192,
-      21440180,
-      21438418,
-      21440684,
-      21439340,
-      21439918,
-      21439089,
-      21439659,
-      21440578,
-      21437551,
-      21437409,
-      21436245,
-      21438588,
-      21437908,
-      21440327,
-      21432480
-    ];
 
-    Future<Null> _updateArticles() async {
-      final futureArticles = _ids.map((id) => _getArticle(id));
-      final articles = await Future.wait(futureArticles);
-      _articles = articles;
-    }
-  
-    Future<Article> _getArticle(int id) async {
-      final storyUrl = 'https://hacker-news.firebaseio.com/v0/item/$id.json';
-      final storyRes = await http.get(storyUrl);
-      if (storyRes.statusCode == 200) {
-        return Article.fromJson(storyRes.body);
+  HackerNewsBloc() {   
+    _getArticlesAndUpdate(_topIds); 
+    _storiesTypeController.stream.listen((storiesType) {
+      List<int> ids;
+      if(storiesType == StoriesType.newStories) {
+        _getArticlesAndUpdate(_newIds);
+      } else {
+        _getArticlesAndUpdate(_topIds);
       }
-    }
-  
+    });
   }
+
+  _getArticlesAndUpdate(List<int> ids) async {
+    _isLoadingSubject.add(true);
+    await _updateArticles(ids).then((_) {
+      _articlesSubject.add(UnmodifiableListView(_articles));
+      _isLoadingSubject.add(false);
+    });
+  }
+
+  static List<int> _newIds = [
+    21440526,
+    21440508,
+    21440582,
+    21437334,
+    21439192,
+    21440180,
+    21438418,
+    21440684,
+    21439340,
+    21439918,
+    21439089
+  ];
+
+  static List<int> _topIds = [
+    21439659,
+    21440578,
+    21437551,
+    21437409,
+    21436245,
+    21438588,
+    21437908,
+    21440327,
+    21432480
+  ];
+
   
-  class Articles {
+
+  Future<Null> _updateArticles(List<int> articleIds) async {
+    final futureArticles = articleIds.map((id) => _getArticle(id));
+    final articles = await Future.wait(futureArticles);
+    _articles = articles;
+  }
+
+  Future<Article> _getArticle(int id) async {
+    final storyUrl = 'https://hacker-news.firebaseio.com/v0/item/$id.json';
+    final storyRes = await http.get(storyUrl);
+    if (storyRes.statusCode == 200) {
+      return Article.fromJson(storyRes.body);
+    }
+  }
 }
+
+class Articles {}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:collection';
 
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:hacker_news/article.dart';
 import 'package:hacker_news/hn_bloc.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Hacker News',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -39,11 +40,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+        leading: LoadingInfo(widget.bloc.isLoading),
       ),
       body: StreamBuilder<UnmodifiableListView<Article>>(
         stream: widget.bloc.articles,
@@ -51,7 +54,30 @@ class _MyHomePageState extends State<MyHomePage> {
         builder: (context, snapshot) => ListView(
           children: snapshot.data.map(_buildItem).toList(),
         ),
-      )
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(
+            title: Text('Top Stories'),
+            icon: Icon(Icons.arrow_drop_up),
+          ),
+          BottomNavigationBarItem(
+            title: Text('New Stories'),
+            icon: Icon(Icons.new_releases),
+          )
+        ],
+        onTap: (index) {
+          if (index == 0) {
+            widget.bloc.storiesType.add(StoriesType.topStories);
+          } else {
+            widget.bloc.storiesType.add(StoriesType.newStories);
+          }
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
       );
   }
 
@@ -81,6 +107,39 @@ class _MyHomePageState extends State<MyHomePage> {
             ],
           )
         ],
+      ),
+    );
+  }
+}
+
+class LoadingInfo extends StatefulWidget {
+  final Stream<bool> isLoading;
+
+  const LoadingInfo(this.isLoading);
+
+  createState() => LoadingInfoState();
+}
+
+class LoadingInfoState extends State<LoadingInfo> with TickerProviderStateMixin{
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this, duration: Duration(seconds: 3));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: StreamBuilder(
+        stream: widget.isLoading,
+        builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+          _controller.forward().then((f) => _controller.reverse());
+            // if (snapshot.hasData && snapshot.data) 
+              return FadeTransition(child: Icon(FontAwesomeIcons.hackerNewsSquare), opacity: _controller,);
+            // else return Container();
+        },
       ),
     );
   }
